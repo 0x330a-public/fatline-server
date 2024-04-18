373e423a0ba47e587c6a4b7bd7177f777df5004b
0x330a <92654767+0x330a@users.noreply.github.com>
2024-04-19 01:51:52 +1000

    feat: rework sig to be byte hash based with timestamp as wip

--- src/auth_layer.rs
+++ src/auth_layer.rs
@@ -1,66 +1,87 @@
 use std::any::Any;
-use std::ops::DerefMut;
 use std::str::FromStr;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
+
 use axum::{
     extract::Request,
-    response::Response
+    response::IntoResponse
 };
 use axum::extract::State;
-use axum::http::{HeaderMap, StatusCode};
+use axum::http::StatusCode;
 use axum::middleware::Next;
-use fatline_rs::{HASH_LENGTH, PUBLIC_KEY_LENGTH, SIGNATURE_LENGTH};
+use fatline_rs::{HASH_LENGTH, HubService, PUBLIC_KEY_LENGTH, SIGNATURE_LENGTH};
 use fatline_rs::users::UserService;
-use tower::{Layer, Service as TowerService};
-use crate::service::Service;
-use crate::{FID_HEADER, PUB_HEX_HEADER, SIGNATURE_DATA_HEADER, SIGNATURE_HEADER};
+use tokio::sync::Mutex;
 
+use crate::{FID_HEADER, PUB_HEX_HEADER, SIGNATURE_DATA_HEADER, SIGNATURE_HEADER, TIMESTAMP_HEADER};
+use crate::service::Service;
+
+// probably move this to some enum class header error and handle it in axum routes zz
 fn bad_request(_: impl Any) -> StatusCode {
     StatusCode::BAD_REQUEST
 }
 
 pub async fn fid_sig_auth_middleware(
-    headers: HeaderMap,
     State(state): State<Arc<Mutex<Service>>>,
     request: Request,
     next: Next,
-) -> Result<Response, StatusCode> {
+) -> Result<impl IntoResponse, StatusCode> {
+    let headers = request.headers();
     // do something with `request`...
-    let msg_header = headers.get(SIGNATURE_DATA_HEADER)
-        .ok_or(StatusCode::BAD_REQUEST)?; // try extract message from header
+    let extra_data_header = headers.get(SIGNATURE_DATA_HEADER); // try extract message from header
     let sig_header = headers.get(SIGNATURE_HEADER)
         .ok_or(StatusCode::BAD_REQUEST)?; // try extract sig from header
     let pub_key_header = headers.get(PUB_HEX_HEADER)
         .ok_or(StatusCode::BAD_REQUEST)?; // try extract pub key from header
+    let timestamp_header = headers.get(TIMESTAMP_HEADER)
+        .ok_or(StatusCode::BAD_REQUEST)?;
     let fid_header = headers.get(FID_HEADER)
         .ok_or(StatusCode::BAD_REQUEST)?; // try extract fid from header
-    let mut msg = [0u8;HASH_LENGTH];
+    let mut extra_data = Vec::new();
     let mut sig = [0u8; SIGNATURE_LENGTH];
     let mut pub_key = [0u8; PUBLIC_KEY_LENGTH];
-
-    hex::decode_to_slice(msg_header.as_bytes(), &mut msg).map_err(bad_request)?;
+    
+    if let Some(data) = extra_data_header {
+        extra_data.append(&mut hex::decode(data.as_bytes()).map_err(bad_request)?);
+    };
+    
     hex::decode_to_slice(sig_header, &mut sig).map_err(bad_request)?;
     hex::decode_to_slice(pub_key_header.as_bytes(), &mut pub_key).map_err(bad_request)?;
-
+    
+    let timestamp_str = timestamp_header.to_str().map_err(bad_request)?;
+    // TODO: check timestamp is withing X minutes / seconds here
+    let timestamp: u64 = u64::from_str(timestamp_str).map_err(bad_request)?;
     let fid: u64 = u64::from_str(fid_header.to_str().map_err(bad_request)?).map_err(bad_request)?;
+    
+    let mut msg = Vec::new();
+    msg.append(&mut Vec::from(pub_key.clone()));
+    msg.append(&mut Vec::from(timestamp_str.as_bytes()));
+    msg.append(&mut extra_data);
+    let msg_hash = fatline_rs::utils::truncated_hash(msg.as_slice());
 
-    let mut service_lock = state.lock().map_err(|_|StatusCode::INTERNAL_SERVER_ERROR)?;
+    let validation_result = {
+        let mut service_lock = state.lock().await;
+        
+        validate_fid_and_key(
+            &mut service_lock.hub_client,
+            msg_hash,
+            sig,
+            pub_key,
+            fid
+        ).await
+    };
 
-    match validate_fid_and_key(
-        &mut service_lock.hub_client,
-        msg,
-        sig,
-        pub_key,
-        fid
-    ).await {
-        Ok(_) => Ok(next.run(request).await),
-        _ => Err(StatusCode::INTERNAL_SERVER_ERROR)
+    if validation_result.is_ok() {
+        let res = next.run(request).await;
+        Ok(res)
+    } else {
+        Err(StatusCode::BAD_REQUEST)
     }
 }
 
 // Validate that pub_key signed the hash and that pub_key belongs to, and is active on fid
 async fn validate_fid_and_key(
-    user_service: &mut dyn UserService,
+    user_service: &mut HubService,
     msg: [u8; HASH_LENGTH],
     sig: [u8; SIGNATURE_LENGTH],
     pub_key: [u8; PUBLIC_KEY_LENGTH],
@@ -86,7 +107,7 @@ async fn validate_fid_and_key(
     // check fid actually has key
 
     if user_service.key_valid_for_fid(&pub_key, fid).await
-        .map_err(|e|StatusCode::INTERNAL_SERVER_ERROR)? {
+        .map_err(|_|StatusCode::INTERNAL_SERVER_ERROR)? {
         Ok(())
     } else {
         Err(StatusCode::BAD_REQUEST)
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
-use std::sync::{Mutex, Arc};
+use std::sync::Arc;
 
-use tokio::net::TcpListener;
-
-use axum::{Router, middleware::{from_fn, from_fn_with_state}, routing::get, http::StatusCode};
+use axum::{middleware::from_fn_with_state, Router, routing::get};
+use axum::http::StatusCode;
 use eyre::Result;
 use fatline_rs::{posts::PostService, users::UserService};
+use tokio::net::TcpListener;
+use tokio::sync::Mutex;
 
-use crate::{service::Service, auth_layer::fid_sig_auth_middleware};
+use crate::{auth_layer::fid_sig_auth_middleware, service::Service};
 
 mod service;
 mod auth_layer;
 
 // constants for headers
+// required headers: pub_hex, timestamp, sig, fid
+// optional header: extra_sig_data, maybe use this as route specific signature verification instead of overall?
+// wip implementation: H(pub_key (not hex) || timestamp str encode bytes || [optional: extra_sig_data (not hex)]) -> should match sig for pub key
 const PUB_HEX_HEADER: &'static str = "key_hex";
-const SIGNATURE_DATA_HEADER: &'static str = "sig_data";
+const TIMESTAMP_HEADER: &'static str = "timestamp";
+const SIGNATURE_DATA_HEADER: &'static str = "extra_sig_data_hex";
 const SIGNATURE_HEADER: &'static str = "sig";
 const FID_HEADER: &'static str = "fid";
 
 #[tokio::main]
 async fn main() -> Result<()> {
-
     let server_url = dotenv::var("SERVER_URL").expect("No server URL included");
     let bind_addr = dotenv::var("BIND_ADDR").unwrap_or("127.0.0.1:8000".to_string());
 
@@ -27,7 +31,7 @@ async fn main() -> Result<()> {
     let service_arc = Arc::new(Mutex::new(service));
 
     let app = Router::new()
-        .route("/", get(test))
+        .route("/", get(|| async { Ok::<(), StatusCode>(()) }))
         .route_layer(from_fn_with_state(service_arc.clone(), fid_sig_auth_middleware))
         .with_state(service_arc);
 
@@ -36,8 +40,4 @@ async fn main() -> Result<()> {
     axum::serve(tcp_listener, app).await?;
 
     Ok(())
-}
-
-async fn test() -> Result<(), StatusCode> {
-    todo!()
 }
