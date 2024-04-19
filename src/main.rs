bdcb1cfd406168ade8a8047dd6221de26f000d0f
0x330a <92654767+0x330a@users.noreply.github.com>
2024-04-19 17:20:39 +1000

    refactor: main endpoint is user profile check

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
+use std::str::FromStr;
 use std::sync::Arc;
 
-use axum::{middleware::from_fn_with_state, Router, routing::get};
-use axum::http::StatusCode;
+use axum::{Json, middleware::from_fn_with_state, Router, routing::get};
+use axum::extract::State;
+use axum::http::{HeaderMap, StatusCode};
+use axum::response::IntoResponse;
 use eyre::Result;
 use fatline_rs::{posts::PostService, users::UserService};
 use tokio::net::TcpListener;
 use tokio::sync::Mutex;
+use serde::Serialize;
 
 use crate::{auth_layer::fid_sig_auth_middleware, service::Service};
 
@@ -22,6 +26,8 @@ const SIGNATURE_DATA_HEADER: &'static str = "extra_sig_data_hex";
 const SIGNATURE_HEADER: &'static str = "sig";
 const FID_HEADER: &'static str = "fid";
 
+type ServiceArcState = State<Arc<Mutex<Service>>>;
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let server_url = dotenv::var("SERVER_URL").expect("No server URL included");
@@ -31,13 +37,24 @@ async fn main() -> Result<()> {
     let service_arc = Arc::new(Mutex::new(service));
 
     let app = Router::new()
-        .route("/", get(|| async { Ok::<(), StatusCode>(()) }))
+        .route("/profile/me", get(current_user_profile))
         .route_layer(from_fn_with_state(service_arc.clone(), fid_sig_auth_middleware))
         .with_state(service_arc);
 
+    println!("Running on {}", &bind_addr);
     let tcp_listener = TcpListener::bind(bind_addr).await.expect("Couldn't create tcp listener");
-
     axum::serve(tcp_listener, app).await?;
 
     Ok(())
+}
+
+
+// TODO: refactor this to use extensions question mark?
+async fn current_user_profile(
+    header_map: HeaderMap,
+    State(service): ServiceArcState,
+) -> Result<Json<fatline_rs::users::Profile>, StatusCode> {
+    let mut service = service.lock().await;
+    let fid = u64::from_str(header_map[FID_HEADER].to_str().expect("Fid header wasn't str")).map_err(|_|StatusCode::INTERNAL_SERVER_ERROR)?;
+    Ok(Json::from(service.hub_client.get_user_profile(fid).await.map_err(|_| StatusCode::NOT_FOUND)?))
 }
